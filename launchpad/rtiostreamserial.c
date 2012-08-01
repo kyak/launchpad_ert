fa64a6ed07abc4c88b8de51d3a042d3168734d53
kyak <bas@bmail.ru>
2012-08-01 18:37:47 +0400

    PIL implementation on Windows is now fully functional
    The root of the problem (not being able to pass anything bigger than 1
    byte through PIL interface) was UART overrun. When a message is big
    enough, the target would overwrite RX buffer with a newer character,
    while the previous one was not read yet. Setting the baud rate to 2400
    fixes the problem.

--- launchpad/rtiostreamserial.c
+++ launchpad/rtiostreamserial.c
@@ -5,8 +5,11 @@
 void FaultRoutine(void);
 void ConfigWDT(void);
 void ConfigClocks(void);
+void usci_init(void);
 void led_init(void);
 void led_toggle(void);
+uint8_T serial_getc();
+void serial_putc(uint8_T c);
 
 /* Receive Data at P1.1 */
 #define RXD		BIT1
@@ -17,62 +20,41 @@ void led_toggle(void);
 /* Initialize Serial */
 int rtIOStreamOpen(int argc, void *argv[])
 {
-    /* ASCII character a is 65 */
+    /* ASCII character A is 65 */
     #define RTIOSTREAM_OPEN_COMPLETE 65
     static const uint8_T init_complete = RTIOSTREAM_OPEN_COMPLETE;
     
     ConfigWDT();
     ConfigClocks();
-
-	P1SEL  = RXD + TXD;                       
-  	P1SEL2 = RXD + TXD;                       
-  	UCA0CTL1 |= UCSSEL_2;                     // SMCLK
-  	UCA0BR0 = 13;                             // 1/8MHz 9600
-  	UCA0BR1 = 0;                              // 1/8MHz 9600
-  	UCA0MCTL = UCBRS0;                        // Modulation UCBRSx = 1
-  	UCA0CTL1 &= ~UCSWRST;                     // Initialize USCI state machine
-  	IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
+    usci_init();
 
     /* send out the initial character */
-    while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
-  	UCA0TXBUF = init_complete;                // TX
+  	serial_putc(init_complete);
+
     led_init();
     return RTIOSTREAM_NO_ERROR;
 }
 
-uint8_T serial_getc()
-{
-    while (!(IFG2&UCA0RXIFG));                // USCI_A0 RX buffer ready?
-	return UCA0RXBUF;
-}
-
 /* Read data from serial */
 int rtIOStreamRecv(
-    int      streamID, // A handle to the stream that was returned by a previous call to rtIOStreamOpen.
-    void   * dst,      // A pointer to the start of the buffer where received data must be copied.
-    size_t   size, // Size of data to copy into the buffer. For byte-addressable architectures,
-                   // size is measured in bytes. Some DSP architectures are not byte-addressable.
-                   // In these cases, size is measured in number of WORDs, where sizeof(WORD) == 1.
+    int      streamID,  // A handle to the stream that was returned by a previous call to rtIOStreamOpen.
+    void   * dst,       // A pointer to the start of the buffer where received data must be copied.
+    size_t   size, 	    // Size of data to copy into the buffer. For byte-addressable architectures,
+                   	    // size is measured in bytes. Some DSP architectures are not byte-addressable.
+                   	    // In these cases, size is measured in number of WORDs, where sizeof(WORD) == 1.
     size_t * sizeRecvd) // The number of units of data received and copied into the buffer dst (zero if no data was copied).
 {
-    *sizeRecvd=0U; // Initialize the number of received chars to zero.
-    //unsigned char *ptr = (unsigned char *)dst; // Initialize ptr is a pointer to the buffer of chars.
     uint8_T *ptr = (uint8_T *)dst; // Initialize ptr is a pointer to the buffer of chars.
-    while (*sizeRecvd < size ) // Try to receive char as many times as commanded.
+
+    *sizeRecvd=0U; // Initialize the number of received chars to zero.
+
+    while (*sizeRecvd < size) // Try to receive char as many times as commanded.
     {
-        *ptr = serial_getc(); // Receive a char into the buffer of chars (as pointed by ptr).
-        ptr++; // Point to the next element of receive buffer.
-        (*sizeRecvd)++; // Increase the number of received chars.
+    	*ptr++ = serial_getc();
+        (*sizeRecvd)++; 		 // Increase the number of received chars.
     }
     led_toggle();
     return RTIOSTREAM_NO_ERROR;
-}
-
-
-void serial_putc(uint8_T c)
-{
-	while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
-  	UCA0TXBUF = c;                    		  // TX
 }
 
 /* Write data to serial */
@@ -86,9 +68,8 @@ int rtIOStreamSend(
     *sizeSent=0U;
     uint8_T *ptr = (uint8_T *)src;
 
-    while((*sizeSent < size)) {
-    	serial_putc(*ptr);
-        ptr++;
+    while (*sizeSent < size) {
+    	serial_putc(*ptr++);
         (*sizeSent)++;
     }
     return RTIOSTREAM_NO_ERROR;
@@ -123,6 +104,17 @@ void FaultRoutine(void)
     while(1); // TRAP
 }
 
+void usci_init(void)
+{
+	P1SEL  = RXD + TXD;
+  	P1SEL2 = RXD + TXD;
+  	UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+  	UCA0BR0 = 52; //2400 see http://www.daycounter.com/Calculators/MSP430-Uart-Calculator.phtml
+  	UCA0BR1 = 0;
+  	UCA0MCTL = 0x20;
+  	UCA0CTL1 &= ~UCSWRST;                     // Initialize USCI state machine
+}
+
 void led_init(void)
 {
     P1DIR |= BIT0;                 // P1.0 output (red LED)
@@ -141,4 +133,16 @@ void led_toggle(void)
         P1OUT &= ~BIT0;                  // red LED off
     }
     led_counter++;
+}
+
+uint8_T serial_getc()
+{
+    while (!(IFG2&UCA0RXIFG));                // USCI_A0 RX buffer ready?
+	return UCA0RXBUF;
+}
+
+void serial_putc(uint8_T c)
+{
+	while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
+  	UCA0TXBUF = c;                    		  // TX
 }
--- launchpad/rtiostreamserial_test.c
+++ launchpad/rtiostreamserial_test.c
@@ -1,128 +1,13 @@
 #include <msp430.h>
 #include "rtiostream.h"
 
-void FaultRoutine(void);
-void ConfigWDT(void);
-void ConfigClocks(void);
-
-/* Receive Data at P1.1 */
-#define RXD		BIT1
-
-/* Transmit Data at P1.2 */
-#define TXD		BIT2
-
-/* Initialize Serial */
-int rtIOStreamOpen(int argc, void *argv[])
-{
-    /* ASCII character a is 65 */
-    #define RTIOSTREAM_OPEN_COMPLETE 65
-    static const unsigned char init_complete = RTIOSTREAM_OPEN_COMPLETE;
-
-    ConfigWDT();
-    ConfigClocks();
-
-	P1SEL  = RXD + TXD;
-  	P1SEL2 = RXD + TXD;
-  	UCA0CTL1 |= UCSSEL_2;                     // SMCLK
-  	UCA0BR0 = 13;                             // 1/8MHz 9600
-  	UCA0BR1 = 0;                              // 1/8MHz 9600
-  	UCA0MCTL = UCBRS0;                        // Modulation UCBRSx = 1
-  	UCA0CTL1 &= ~UCSWRST;                     // Initialize USCI state machine
-  	IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
-
-    /* send out the initial character */
-    while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
-  	UCA0TXBUF = init_complete;                // TX
-    return RTIOSTREAM_NO_ERROR;
-}
-
-unsigned char serial_getc()
-{
-    while (!(IFG2&UCA0RXIFG));                // USCI_A0 RX buffer ready?
-	return UCA0RXBUF;
-}
-
-/* Read data from serial */
-int rtIOStreamRecv(
-    int      streamID, // A handle to the stream that was returned by a previous call to rtIOStreamOpen.
-    void   * dst,      // A pointer to the start of the buffer where received data must be copied.
-    size_t   size, // Size of data to copy into the buffer. For byte-addressable architectures,
-                   // size is measured in bytes. Some DSP architectures are not byte-addressable.
-                   // In these cases, size is measured in number of WORDs, where sizeof(WORD) == 1.
-    size_t * sizeRecvd) // The number of units of data received and copied into the buffer dst (zero if no data was copied).
-{
-    *sizeRecvd=0U; // Initialize the number of received chars to zero.
-    unsigned char *ptr = (unsigned char *)dst; // Initialize ptr is a pointer to the buffer of chars.
-    while (*sizeRecvd < size ) // Try to receive char as many times as commanded.
-    {
-        *ptr = serial_getc(); // Receive a char into the buffer of chars (as pointed by ptr).
-        ptr++; // Point to the next element of receive buffer.
-        (*sizeRecvd)++; // Increase the number of received chars.
-    }
-    return RTIOSTREAM_NO_ERROR;
-}
-
-
-void serial_putc(unsigned char c)
-{
-	while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
-  	UCA0TXBUF = c;                    		  // TX
-}
-
-/* Write data to serial */
-int rtIOStreamSend(
-    int          streamID,
-    const void * src,
-    size_t       size,
-    size_t     * sizeSent)
-{
-
-    *sizeSent=0U;
-    unsigned char *ptr = (unsigned char *)src;
-
-    while((*sizeSent < size)) {
-    	serial_putc(*ptr);
-        ptr++;
-        (*sizeSent)++;
-    }
-    return RTIOSTREAM_NO_ERROR;
-}
-
-int rtIOStreamClose(int streamID)
-{
-    return RTIOSTREAM_NO_ERROR;
-}
-
-void ConfigWDT(void)
-{
-    WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
-}
-
-void ConfigClocks(void)
-{
-    if (CALBC1_1MHZ ==0xFF || CALDCO_1MHZ == 0xFF)
-    FaultRoutine(); // If calibration data is erased
-                    // run FaultRoutine()
-    BCSCTL1 = CALBC1_1MHZ; // Set range
-    DCOCTL = CALDCO_1MHZ; // Set DCO step + modulation
-    BCSCTL3 |= LFXT1S_2; // LFXT1 = VLO
-    IFG1 &= ~OFIFG; // Clear OSCFault flag
-    BCSCTL2 |= SELM_0 + DIVM_3 + DIVS_3; // MCLK = DCO/8, SMCLK = DCO/8
-    //BCSCTL2 = 0; //to drive MCLK and SMCLK directly from DCO
-}
-
-void FaultRoutine(void)
-{
-    P1OUT = BIT0; // P1.0 on (red LED)
-    while(1); // TRAP
-}
-
 void serial_simple_step(void)
 {
 
 #define MAX_PACKET_SIZE 50
 
     unsigned char data[MAX_PACKET_SIZE];
+	//unsigned short data[MAX_PACKET_SIZE];
 
     static unsigned int packetSize=1;
     unsigned int i;
